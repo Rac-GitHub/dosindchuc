0bb61a6e1f2c81153bc5f3212db86a40c543c337
paulo.rachinhas@gmail.com <paulo.rachinhas@gmail.com@32eba0d8-e76c-5bae-aa7d-a8390c808913>
2013-11-07 12:59:05 +0000

    Intro de entities

---
+++ DosIndCHUC/src/dosindchuc/entities/create_enums.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dosindchuc.entities;
+
+/**
+ *
+ * @author ir
+ */
+public class create_enums {
+    
+    public enum categoryType {
+         Médico 
+        , Físico
+        , Técnico
+        , Secretário
+        , Enfermeiro
+        , Auxiliar 
+    }
+    
+    public enum sexType { M , F }
+    
+    
+    public enum departmentType {
+         Radioterapia 
+        , Imagiologia
+        , Ortopedia
+        , BOC
+        , MedicinaNuclear
+    }
+    
+    public enum statusType { Activo , Inactivo }
+    
+}
---
+++ DosIndCHUC/src/dosindchuc/entities/dose_info.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dosindchuc;
+
+/**
+ *
+ * @author ir
+ */
+public class dose_info {
+    
+}
